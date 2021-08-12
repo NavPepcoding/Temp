fa0456f1b8cd408b956484b1e583b79187bffcbd
amaan-ahmad <amaanahmad@bpitindia.com>
2021-08-12 15:28:00 +0530

    Remove Logs and Error Handling

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const users = require('./models/users')
 const app = express()
 const server = require('http').createServer(app)
+require('dotenv/config')
+
 const io = require('socket.io')(server, {
     cors: {
         orign: "*"
     }
 })
 const PORT = process.env.PORT || 2000
-require('dotenv/config')
 const mongoose = require('mongoose')
 const messages = require('./models/messages')
 const rooms = require('./models/rooms')
@@ -24,41 +25,52 @@ io.on('connection', (socket) => {
     })
 
     socket.on('send-message', async (data) => {
-        const { from, msg, roomID } = data;
-        const roomDoc = await rooms.findById(roomID)
-        let { read, p1, p2 } = roomDoc;
+        try {
+            const { from, msg, roomID } = data;
+            const roomDoc = await rooms.findById(roomID)
+            let { read, p1, p2 } = roomDoc;
 
-        const to = p1.equals(mongoose.Types.ObjectId(from)) ? p2 : p1;
-        let newMessage = new messages({
-            roomID,
-            from,
-            to,
-            msg
-        })
-        newMessage.save();
-        read = {
-            p1: mongoose.Types.ObjectId(from).equals(p1),
-            p2: mongoose.Types.ObjectId(from).equals(p2),
+            const to = p1.equals(mongoose.Types.ObjectId(from)) ? p2 : p1;
+            let newMessage = new messages({
+                roomID,
+                from,
+                to,
+                msg
+            })
+            newMessage.save();
+            read = {
+                p1: mongoose.Types.ObjectId(from).equals(p1),
+                p2: mongoose.Types.ObjectId(from).equals(p2),
+            }
+            await rooms.findOneAndUpdate({ _id: roomID }, {
+                read
+            })
+            socket.to(JSON.stringify(roomID)).emit('get-message', { ...data, to })
         }
-        await rooms.findOneAndUpdate({ _id: roomID }, {
-            read
-        })
-        socket.to(JSON.stringify(roomID)).emit('get-message', { ...data, to })
+        catch (err) {
+            console.log(err)
+        }
     })
 
     socket.on('update-read', async (data) => {
-        const { roomID, user } = data
-        const room = await rooms.findById(data.roomID);
-        let { p1, read } = room;
-        if (mongoose.Types.ObjectId(user).equals(p1))
-            read = { ...read, p1: true };
-        else read = { ...read, p2: true }
-        await rooms.findOneAndUpdate({
-            _id: roomID
-        }, {
-            read
-        })
+        try {
+            const { roomID, user } = data
+            const room = await rooms.findById(data.roomID);
+            let { p1, read } = room;
+            if (mongoose.Types.ObjectId(user).equals(p1))
+                read = { ...read, p1: true };
+            else read = { ...read, p2: true }
+            await rooms.findOneAndUpdate({
+                _id: roomID
+            }, {
+                read
+            })
+        }
+        catch (err) {
+            console.log(err)
+        }
     })
+
 })
 
 app.use(cors())
@@ -66,7 +78,7 @@ mongoose.connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ignoreUndefined: true,
-    useFindAndModify : true
+    useFindAndModify: false
 })
     .then(() => {
         console.log("DB connected")
@@ -76,66 +88,79 @@ mongoose.connect(process.env.MONGO_DB_URI, {
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
 
 app.post("/user/create", async function (req, res) {
-    const user = await users.findOne({ email: req.body.email })
-    if (!user) {
-        const newUser = new users({
-            email: req.body.email
-        })
-        newUser.save();
-        const userId = newUser._id;
-
-
-        users.find().where("email").ne(req.body.email).then(users => {
-            users.forEach(user => {
-                const newRoom = new rooms({
-                    p1: mongoose.Types.ObjectId(userId),
-                    p2: mongoose.Types.ObjectId(user._id)
-                })
-                newRoom.save()
+    try {
+        const { email } = req.body
+        if (!email || email.length === 0)
+            return res.status(500).json("invalid email")
+        const user = await users.findOne({ email: req.body.email })
+        if (!user) {
+            const newUser = new users({
+                email: req.body.email
             })
-        })
-        res.json(newUser)
-    } else {
-        res.json(user);
+            newUser.save();
+            const userId = newUser._id;
+
+
+            users.find().where("email").ne(req.body.email).then(users => {
+                users.forEach(user => {
+                    const newRoom = new rooms({
+                        p1: mongoose.Types.ObjectId(userId),
+                        p2: mongoose.Types.ObjectId(user._id)
+                    })
+                    newRoom.save()
+                })
+            })
+            res.json(newUser)
+        } else {
+            res.json(user);
+        }
+    }
+    catch (err) {
+        res.sendStatus(500)
     }
 
 })
 app.get('/users/:id', async function (req, res) {
-    const currUserId = req.params.id;
-
-    const roomList = await rooms.find({ $or: [{ p1: currUserId }, { p2: currUserId }] }).sort({ updatedAt: -1 })
-    const roomArrPromise = roomList.map(async (room) => {
-        return {
-            roomID: room._id,
-            email: (await users.findOne().where("_id").equals(room.p1 == currUserId ? room.p2 : room.p1)).email,
-            read: room.p1 == currUserId ? room.read.p1 : room.read.p2,
-            _id: (await users.findOne().where("_id").equals(room.p1 == currUserId ? room.p2 : room.p1))._id,
-            updatedAt: room.updatedAt
-        }
-    })
-    const roomArr = await Promise.all(roomArrPromise)
-    res.json(roomArr)
+    try {
+        const currUserId = req.params.id;
+        const roomList = await rooms.find({ $or: [{ p1: currUserId }, { p2: currUserId }] }).sort({ updatedAt: -1 })
+        const roomArrPromise = roomList.map(async (room) => {
+            return {
+                roomID: room._id,
+                email: (await users.findOne().where("_id").equals(room.p1 == currUserId ? room.p2 : room.p1)).email,
+                read: room.p1 == currUserId ? room.read.p1 : room.read.p2,
+                _id: (await users.findOne().where("_id").equals(room.p1 == currUserId ? room.p2 : room.p1))._id,
+                updatedAt: room.updatedAt
+            }
+        })
+        const roomArr = await Promise.all(roomArrPromise)
+        res.json(roomArr)
+    }
+    catch (err) {
+        res.sendStatus(500)
+    }
 
 })
 
 
 app.get("/message/:roomID", function (req, res) {
-    // find room id using p1 and p2
-    const { roomID } = req.params
-    messages
-        .find({ roomID })
-        .sort({ createdAt: 1 })
-        .exec(function (err, docs) {
-            if (err) {
-                return res.sendStatus(500)
-            }
-            return res.json(docs);
-        })
+    try {
+        const { roomID } = req.params
+        messages
+            .find({ roomID })
+            .sort({ createdAt: 1 })
+            .exec(function (err, docs) {
+                if (err) {
+                    return res.sendStatus(500)
+                }
+                return res.json(docs);
+            })
+    }
+    catch (err) {
+        res.sendStatus(500)
+    }
 })
 
 server.listen(PORT, () => {
